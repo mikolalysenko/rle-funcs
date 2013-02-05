a25be84712fad82170b3b8098266f531d2b47dc4
Mikola Lysenko <mikolalysenko@gmail.com>
2013-02-05 09:47:03 -0800

    fixed some bugs in implementation

--- index.js
+++ index.js
@@ -1,67 +1,49 @@
 "use strict"; "use restrict";
 
 var core              = require("rle-core");
+var saturateAbs       = core.saturateAbs;
 var removeDuplicates  = require("rle-repair").removeDuplicates;
 
 //Most general function: Performs a k-way merge on a collection of volumes
 exports.merge = function(volumes, stencil, merge_func) {
   var result      = new core.DynamicVolume()
     , iter        = core.beginMulti(volumes, stencil)
-    , ptrs        = iter.ptrs
-    , stencil_len = iter.stencil_len
     , coord       = iter.coord
-    , phases      = new Int32Array(ptrs.length)
-    , distances   = new Float64Array(ptrs.length)
-    , retval      = [0, 1.0];
+    , phases      = new Int32Array(iter.ptrs.length)
+    , distances   = new Float64Array(iter.ptrs.length)
+    , retval      = [ 0, 1.0 ];
   while(iter.hasNext()) {
-    for(var i=0, idx=0; i<volumes.length; ++i) {
-      var vphases     = volumes[i].phases
-        , vdistances  = volumes[i].distances;
-      for(var j=0; j<stencil_len; ++j, ++idx) {
-        var p = ptrs[idx];
-        phases[idx]     = vphases[p];
-        distances[idx]  = vdistances[p];
-      }
-    }
+    iter.getValues(phases, distances);
     merge_func(phases, distances, retval);
-    result.push(coord[0], coord[1], coord[2], retval[1], retval[0]);
+    result.push(coord[0], coord[1], coord[2], saturateAbs(retval[1]), retval[0]);
     iter.next();
   }
   return removeDuplicates(result);
 }
 
-//Optimization: merge for a trivial stencil
+//Optimization: merge for a point stencil
 exports.mergePoint = function(volumes, merge_func) {
   return exports.merge(volumes, new Int32Array(3), merge_func);
 }
 
 //Optimization:  merge for a sungle volume
 exports.apply = function(volume, stencil, func) {
-  var vdistances  = volume.distances
-    , vphases     = volume.phases
-    , stencil_len = (stencil.length / 3) | 0
-    , distances   = new Float64Array(stencil_len)
-    , phases      = new Int32Array(stencil_len)
-    , nvolume     = new core.DynamicVolume()
-    , retval      = [ 0, 1.0 ]
+  var result      = new core.DynamicVolume()
     , iter        = core.beginStencil(volume, stencil)
-    , ptrs        = iter.ptrs
-    , c           = iter.coord;
-  nvolume.pop()
+    , coord       = iter.coord
+    , phases      = new Int32Array(iter.ptrs.length)
+    , distances   = new Float64Array(iter.ptrs.length)
+    , retval      = [ 0, 1.0 ];
   while(iter.hasNext()) {
-    for(var i=0; i<stencil_len; ++i) {
-      var p = ptrs[i];
-      distances[i]  = vdistances[p];
-      phases[i]     = vphases[p];
-    }
+    iter.getValues(phases, distances);
     func(phases, distances, retval);
-    nvolume.push(c[0], c[1], c[2], retval[1], retval[0]);
+    result.push(coord[0], coord[1], coord[2], saturateAbs(retval[1]), retval[0]);
     iter.next();
   }
-  return removeDuplicates(nvolume);
+  return removeDuplicates(result);
 }
 
-//Optimization merge for a pointwise function
+//Optimization: apply for a point stencil
 exports.applyPoint = function(volume, func) {
   var nvolume = volume.clone()
     , nruns   = volume.length()
@@ -69,7 +51,7 @@ exports.applyPoint = function(volume, func) {
   for(var i=0; i<nruns; ++i) {
     func(nvolume.phases[i], nvolume.distances[i], retval)
     nvolume.phases[i]     = retval[0];
-    nvolume.distances[i]  = retval[1];
+    nvolume.distances[i]  = saturateAbs(retval[1]);
   }
   return removeDuplicates(nvolume);
 }
